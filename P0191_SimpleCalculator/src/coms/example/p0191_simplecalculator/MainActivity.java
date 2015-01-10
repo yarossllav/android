db3f7f00450a214b04bb24be5c5d8085968c3381
Yaroslav <yarossllav@gmail.com>
2015-01-10 13:43:27 +0200

    add menu

--- P0191_SimpleCalculator/src/coms/example/p0191_simplecalculator/MainActivity.java
+++ P0191_SimpleCalculator/src/coms/example/p0191_simplecalculator/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 	Menu menu;
@@ -78,9 +79,11 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+				
+		menu.add(0, 1, 1, "Reset");
+		menu.add(0, 2, 2, "Quit");
 		
+		return super.onCreateOptionsMenu(menu);
 		
 	}
 
@@ -89,10 +92,18 @@ public class MainActivity extends Activity implements OnClickListener {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		switch (item.getItemId()){
+		case 1:
+			etNum1.setText("0");
+			etNum2.setText("0");
+			Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+			break;
+		case 2:
+			Toast.makeText(this, "Quit", Toast.LENGTH_SHORT).show();
+			finish();
 			return true;
 		}
+					
 		return super.onOptionsItemSelected(item);
 	}
 
