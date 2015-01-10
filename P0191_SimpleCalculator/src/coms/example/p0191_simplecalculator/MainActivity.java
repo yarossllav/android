2b8c54a697cebe7cefa363da37d11b8641cafb2e
Yaroslav <yarossllav@gmail.com>
2015-01-10 15:07:47 +0200

    work variant

--- P0191_SimpleCalculator/src/coms/example/p0191_simplecalculator/MainActivity.java
+++ P0191_SimpleCalculator/src/coms/example/p0191_simplecalculator/MainActivity.java
@@ -2,6 +2,8 @@ package coms.example.p0191_simplecalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +22,9 @@ public class MainActivity extends Activity implements OnClickListener {
 	Button btnMult;
 	Button btnDiv;
 	TextView tvResult;
+	
+	final int MENU_RESET_ID = 1;
+	final int MENU_QUI_ID = 2;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +54,16 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		
+		if (TextUtils.isEmpty(etNum1.getText().toString()) || TextUtils.isEmpty(etNum2.getText().toString())) {
+			Toast.makeText(this, "Error: add the number!", Toast.LENGTH_LONG);
+			return;
+		}
+		
 		float number1 = Float.parseFloat(etNum1.getText().toString());
 		float number2 = Float.parseFloat(etNum2.getText().toString());
 		float result = 0;
-		String znak = " ";
-						
+		String znak = "";
+		
 		switch (v.getId()){
 		case R.id.btnAdd:
 			znak = "+";
@@ -68,6 +78,10 @@ public class MainActivity extends Activity implements OnClickListener {
 			result = number1 * number2;
 			break;
 		case R.id.btnDiv:
+			if (Float.parseFloat(etNum2.getText().toString())==0){
+				Toast.makeText(this, "Error: div for 0", Toast.LENGTH_SHORT).show();
+				return;
+			}
 			znak = "/";
 			result = number1 / number2;
 			break;
@@ -80,8 +94,8 @@ public class MainActivity extends Activity implements OnClickListener {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 				
-		menu.add(0, 1, 1, "Reset");
-		menu.add(0, 2, 2, "Quit");
+		menu.add(0, MENU_RESET_ID, 1, "Reset");
+		menu.add(0, MENU_QUI_ID, 2, "Quit");
 		
 		return super.onCreateOptionsMenu(menu);
 		
@@ -93,12 +107,13 @@ public class MainActivity extends Activity implements OnClickListener {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		switch (item.getItemId()){
-		case 1:
-			etNum1.setText("0");
-			etNum2.setText("0");
+		case MENU_RESET_ID:
+			etNum1.setText("");
+			etNum2.setText("");
+			tvResult.setText("");
 			Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
 			break;
-		case 2:
+		case MENU_QUI_ID:
 			Toast.makeText(this, "Quit", Toast.LENGTH_SHORT).show();
 			finish();
 			return true;
